f3920cb12a841c359274bc79622d92b72f9f71ec
Danubia Camargo <danubiabcamargo@gmail.com>
2023-10-16 22:15:27 -0300

    atividade validacao formulario

---
+++ javascript/atividades/js/atv35.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', fuction(){
+    const form = document.getElementById('form');
+    const nomeUsuarioInput =  document.getElementById('nomeusuario');
+    const erroElement = document.getElementById('erro');
+
+    form.addEventListener('submit', function (event){
+
+
+    });
+
+
+
+});
